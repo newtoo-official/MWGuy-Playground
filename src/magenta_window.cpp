40ceb10bab525c8fefa49a0c81f24f9bb56145f6
mwguy <yousan4ik@gmail.com>
2019-02-19 00:07:15 +0300

    Update

--- src/magenta_window.cpp
+++ src/magenta_window.cpp
@@ -16,12 +16,21 @@ GtkWindow * magenta_create_styled_window() {
 
 GtkWindow * magenta_create_main_window() {
     GtkWindow * window = magenta_create_styled_window();
-    gtk_window_set_title(window, "Magenta");
+    gtk_header_bar_set_custom_title(GTK_HEADER_BAR(gtk_window_get_titlebar(window)), gtk_label_new_with_mnemonic("Magenta"));
+    gtk_window_set_position(window, GTK_WIN_POS_CENTER);
+    gtk_window_set_default_size(window, 850, 600);
 
     WebKitWebView * view = WEBKIT_WEB_VIEW(webkit_web_view_new());
-    webkit_web_view_load_uri(view, "http://newtoo.ru/magenta/ru");
+    webkit_web_view_load_uri(view, "https://github.com/newtoo-official/Magenta");
+    WebKitWebView * view2 = WEBKIT_WEB_VIEW(webkit_web_view_new());
+    webkit_web_view_load_uri(view2, "http://newtoo.ru/magenta/ru/");
 
-    gtk_container_add(GTK_CONTAINER(window), GTK_WIDGET(view));
+
+    GtkNotebook * notebook = GTK_NOTEBOOK(gtk_notebook_new());
+    gtk_notebook_append_page(notebook, GTK_WIDGET(view), gtk_label_new_with_mnemonic("Test"));
+    gtk_notebook_append_page(notebook, GTK_WIDGET(view2), gtk_label_new_with_mnemonic("Test"));
+
+    gtk_container_add(GTK_CONTAINER(window), GTK_WIDGET(notebook));
 
     return window;
 }
