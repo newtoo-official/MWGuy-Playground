0733e0088f7598ac733acd829af971c2a3540797
mwguy <yousan4ik@gmail.com>
2019-02-19 01:30:15 +0300

    Add tabs (part one of XXX)

--- src/magenta_window.c
+++ src/magenta_window.c
@@ -1,4 +1,5 @@
 #include "magenta_window.h"
+#include "magenta_tabs.h"
 
 GtkWindow * magenta_create_styled_window() {
     GtkWindow * window = GTK_WINDOW(gtk_window_new(GTK_WINDOW_TOPLEVEL));
@@ -18,19 +19,12 @@ GtkWindow * magenta_create_main_window() {
     GtkWindow * window = magenta_create_styled_window();
     gtk_header_bar_set_custom_title(GTK_HEADER_BAR(gtk_window_get_titlebar(window)), gtk_label_new_with_mnemonic("Magenta"));
     gtk_window_set_position(window, GTK_WIN_POS_CENTER);
-    gtk_window_set_default_size(window, 850, 600);
+    gtk_window_set_default_size(window, 1100, 600);
 
-    WebKitWebView * view = WEBKIT_WEB_VIEW(webkit_web_view_new());
-    webkit_web_view_load_uri(view, "https://github.com/newtoo-official/Magenta");
-    WebKitWebView * view2 = WEBKIT_WEB_VIEW(webkit_web_view_new());
-    webkit_web_view_load_uri(view2, "http://newtoo.ru/magenta/ru/");
+    int id = magenta_tabs_build_tab_pane();
+    int tab_id = magenta_tab_open_uri(id, "https://github.com/newtoo-official/Magenta");
 
-
-    GtkNotebook * notebook = GTK_NOTEBOOK(gtk_notebook_new());
-    gtk_notebook_append_page(notebook, GTK_WIDGET(view), gtk_label_new_with_mnemonic("Test"));
-    gtk_notebook_append_page(notebook, GTK_WIDGET(view2), gtk_label_new_with_mnemonic("Test"));
-
-    gtk_container_add(GTK_CONTAINER(window), GTK_WIDGET(notebook));
+    gtk_container_add(GTK_CONTAINER(window), GTK_WIDGET(magenta_get_tab_pane_widget(id)));
 
     return window;
 }
